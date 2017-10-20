ce04bc3ae9c65f758984153512beb6324996c504
NickB <nbrutschy@gmail.com>
2017-10-20 10:50:39 -0700

    finished project

--- Chick.java
+++ Chick.java
@@ -1,4 +1,17 @@
-class Chick //implements Animal 
-{     
-     //your code here
+class Chick implements Animal {
+  private String myType;
+  private String mySound;
+  public Chick(String type, String sound1, String sound2) {
+    myType = type;
+    if(Math.random()>.5)
+        mySound = sound1;
+     else 
+       mySound = sound2;
+  }
+  public String getSound() {
+    return mySound;
+  }
+  public String getType() {
+    return myType;
+  }
 }
--- OldMacDonald.pde
+++ OldMacDonald.pde
@@ -1,6 +1,5 @@
-
 void setup()
 {
-	//your variable declarations here
+  Farm farm1 = new Farm();
+  farm1.animalSounds();
 }
-
